af3a1ff6bca3db088e881fb24b109456391f4439
Eduardo R. Duraes <eduardoduraes.bsi@gmail.com>
2024-04-16 23:29:34 -0300

    modify file cors config 4

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -5,15 +5,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${cors.originPatterns:default}")
-    private String corsOriginPatterns = "";
+
+    private String corsOriginPatterns = "https://bankline-api-production-3838.up.railway.app";
 
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         var allowOrigins = corsOriginPatterns.split(",");
         registry.addMapping("/**")
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedMethods("GET","POST","PUT")
                 .allowedMethods("*")
                 .allowedOrigins(allowOrigins)
                 .allowCredentials(true);
