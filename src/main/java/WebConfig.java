8fe1a1b313ddd347d82b37e3b34faea2a19bc5ae
Eduardo R. Duraes <eduardoduraes.bsi@gmail.com>
2024-04-18 23:33:04 -0300

    atler cors 21

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -1,20 +1,21 @@
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.originPatterns:default}")
-    private String corsOriginPatterns = "";
+//    @Value("${cors.originPatterns:default}")
+//    private String corsOriginPatterns = "";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        var allowOrigins = corsOriginPatterns.split(",");
+//        var allowOrigins = corsOriginPatterns.split(",");
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins(allowOrigins)
-                .allowCredentials(true);
+                .allowedOrigins("*")
+                .allowCredentials(false);
     }
 }
